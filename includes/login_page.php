024f1dd2e34088a519f10e9fc0200fd5597ba640
Erik Uunila <me@erikuunila.com>
2015-03-10 10:50:31 -0500

    Added non-stylesheet login page logo replacement

--- includes/login_page.php
+++ includes/login_page.php
@@ -1,9 +1,25 @@
 <?php
 
+function custom_login_logo() { ?>
+	<style type="text/css">
+		h1 a {
+			background-image:url('<?php get_template_directory_uri() ?>/images/logo.png');
+			padding-bottom: 30px;
+			background-size: initial;
+			width: auto;
+			margin-bottom: 0;
+		}
+	</style>
+<?php }
+
+add_filter( 'login_head', 'custom_login_logo' );
+
+
 // Change login page logo link
 function custom_login_url(){
 	return get_bloginfo('url');
 }
+
 add_filter('login_headerurl', 'custom_login_url');
 
 
@@ -11,13 +27,15 @@ add_filter('login_headerurl', 'custom_login_url');
 function custom_login_title() {
 	return get_bloginfo('name');
 }
+
 add_filter('login_headertitle', 'custom_login_title');
 
 
+/*
 // Add custom CSS & JS to login page
-// Use the CSS to change the logo
 function custom_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/stylesheets/css/login-page.css' );
     wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/js/src/login-page.js' );
 }
 add_action( 'login_enqueue_scripts', 'custom_login_stylesheet' );
+*/
