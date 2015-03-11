e2695b580a09aa9f9b879fbe7f0bb8cb1f8744ef
Erik Uunila <me@erikuunila.com>
2015-03-11 10:14:59 -0500

    Added non-stylesheet logo replacement. Also added a if file exists check.

--- includes/login_page.php
+++ includes/login_page.php
@@ -1,25 +1,28 @@
 <?php
-
-function custom_login_logo() { ?>
-	<style type="text/css">
-		h1 a {
-			background-image:url('<?php get_template_directory_uri() ?>/images/logo.png');
-			padding-bottom: 30px;
-			background-size: initial;
-			width: auto;
-			margin-bottom: 0;
-		}
-	</style>
-<?php }
+function custom_login_logo() {
+	$logo = '/images/sprites/common-1x/logo.png';
+	$logoPath = get_template_directory() . $logo;
+	$logoURL = get_template_directory_uri() . $logo;
+	if (file_exists($logoPath)) { ?>
+		<style type="text/css">
+			.login h1 a {
+				background-image:url('<?php echo $logoURL; ?>');
+				height: 92px; /* set this to height of logo image */
+				width: auto;
+				margin-bottom: 0;
+				background-size: initial;
+				padding-bottom: 20px;
+			}
+		</style>
+	<?php }
+}
 
 add_filter( 'login_head', 'custom_login_logo' );
-
 
 // Change login page logo link
 function custom_login_url(){
 	return get_bloginfo('url');
 }
-
 add_filter('login_headerurl', 'custom_login_url');
 
 
@@ -27,12 +30,11 @@ add_filter('login_headerurl', 'custom_login_url');
 function custom_login_title() {
 	return get_bloginfo('name');
 }
-
 add_filter('login_headertitle', 'custom_login_title');
 
 
+// Uncomment the following to use custom CSS & JS on the login page
 /*
-// Add custom CSS & JS to login page
 function custom_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/stylesheets/css/login-page.css' );
     wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/js/src/login-page.js' );
