24e9c1b7d3547fa1226490c6fb24fac45b46fdf2
Nathan Shubert-Harbison <nathan@domain7.com>
2015-04-02 14:49:47 -0700

    cleaned up functions.php a bit, load template functions with glob()

--- functions.php
+++ functions.php
@@ -2,19 +2,19 @@
 
 // Includes
 include_once 'includes/theme_setup.php'; // stylesheet_uri, after_setup_theme, cleanup head
+include_once 'includes/sidebars.php'; // core sidebar registration
+include_once 'includes/menus.php'; // core menu registration
+include_once 'includes/image_sizes.php'; // image size definitions
 include_once 'includes/scripts.php'; // Enqued Scripts
-include_once 'includes/body_class.php'; // Add page slug as body class. Also include the page parent
-require_once 'includes/post_classes.php';
-require_once 'includes/menu_classes.php';
-include_once 'includes/menus.php';
-include_once 'includes/image_sizes.php';
-include_once 'includes/sidebars.php';
-include_once 'includes/cpt_archives.php';
-include_once 'includes/login_page.php';
+include_once 'includes/cpt_archives.php'; // Add custom post types to wp_get_archives
+include_once 'includes/login_page.php'; // Customize admin login
+include_once 'includes/body_class.php'; // Expand body classes
+require_once 'includes/post_classes.php'; // Expand post classes
+require_once 'includes/menu_classes.php'; // Expand menu classes
 
-// Template functions
-require_once 'includes/template_functions/custom_excerpt.php';
-require_once 'includes/template_functions/document_title.php';
-require_once 'includes/template_functions/custom_fields.php';
+// Template functions. If order is important, replace this and require each file separately.
+foreach (glob(dirname(__FILE__) . '/includes/template_functions/*.php') as $filename) {
+	require_once($filename);
+}
 
 ?>
