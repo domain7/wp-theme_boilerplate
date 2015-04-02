63ce8536d7e57bf2457e99019ac329cb0415546d
Nathan Shubert-Harbison <nathan@domain7.com>
2015-04-01 18:30:16 -0700

    Moved post_classes.php out of template functions

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 include_once 'includes/theme_setup.php'; // stylesheet_uri, after_setup_theme, cleanup head
 include_once 'includes/scripts.php'; // Enqued Scripts
 include_once 'includes/bodyclass.php'; // Add page slug as body class. Also include the page parent
+require_once 'includes/post_classes.php';
 include_once 'includes/menus.php';
 include_once 'includes/image_sizes.php';
 include_once 'includes/sidebars.php';
@@ -15,6 +16,5 @@ require_once 'includes/template_functions/custom_excerpt.php';
 require_once 'includes/template_functions/document_title.php';
 require_once 'includes/template_functions/page_title.php';
 require_once 'includes/template_functions/custom_fields.php';
-require_once 'includes/template_functions/post_classes.php';
 
 ?>
