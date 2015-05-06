bf626e6fed9a1c2500389dc43e066fc43d41ee2a
Erik Uunila <me@erikuunila.com>
2015-05-06 17:42:59 -0500

    Added weight for adding theme style so it loads after all other styles

--- includes/theme_setup.php
+++ includes/theme_setup.php
@@ -46,12 +46,13 @@ function d7_stylesheet_uri($stylesheet_uri, $stylesheet_dir_uri){
  * @subpackage boilerplate-theme\filters+hooks
  * @uses d7_stylesheet_uri()
  * @link http://codex.wordpress.org/Function_Reference/wp_enqueue_style
+ * @internal the '15' in the add_action forces the file to load after the default styles.
  *
  */
 function d7_enqueue_styles() {
     wp_enqueue_style('theme-stylesheet',  d7_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'd7_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'd7_enqueue_styles', 15 );
 
 // Clean up <head> and improve security.
 remove_action('wp_head', 'rsd_link');
